067c009a17510aa6800c9f1b4f3b6b2f11676f70
bakoe <ba.koe96@gmail.com>
2019-01-23 17:20:05 +0100

    Fix inconsistent usage of of "Phone” vs. "Mobile"

--- src/WebsiteInteractionSteps.php
+++ src/WebsiteInteractionSteps.php
@@ -27,18 +27,18 @@ trait WebsiteInteractionSteps
      *   protected $defaultResponsiveViewportWidths = array(
      *        'Desktop' => 1365,
      *        'Tablet'  => 1023,
-     *        'Phone'   => 640
+     *        'Mobile'  => 640
      *   );
      *
-     * @param string $deviceType one of "Desktop", "Tablet" or "Phone"
-     * @return array a mapping of the viewport breakpoints "Desktop", "Tablet" and "Phone" to their corresponding widths
+     * @param string $deviceType one of "Desktop", "Tablet" or "Mobile"
+     * @return array a mapping of the viewport breakpoints "Desktop", "Tablet" and "Mobile" to their corresponding widths
      */
     public function getDefaultResponsiveViewportWidths($deviceType)
     {
         $widthMapping = isset($this->defaultResponsiveViewportWidths) ? $this->defaultResponsiveViewportWidths : array(
             'Desktop' => 1823,
             'Tablet'  => 1023,
-            'Phone'   => 767
+            'Mobile'  => 767
         );
         return $widthMapping[$deviceType];
     }
